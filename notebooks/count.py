8dd9355014aaeae9f9e8a83e70921a084f48e926
Denis Porsev <den.porsev@gmail.com>
2024-02-07 01:08:52 +0300

    Solved HW1 block 2.1

---
+++ notebooks/count.py
@@ -0,0 +1,26 @@
+from mrjob.job import MRStep, MRJob
+
+class MRWordCount(MRJob):
+    def map(self, _, line):
+        parsed = (line.lower()).replace('"', '').split()[1:]
+        yield parsed[0], 1
+
+    def reduce(self, characters, values):
+        yield characters, sum(values)
+
+    def map_best(self, characters, count):
+        yield None, (count, characters)
+
+    def reduce_best(self, _, phrases_count):
+        for count, characters in sorted(phrases_count, reverse=True)[:20]:
+            yield characters, count
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.map, reducer=self.reduce),
+            MRStep(mapper=self.map_best, reducer=self.reduce_best)
+        ]
+
+
+if __name__ == "__main__":
+    MRWordCount.run()
